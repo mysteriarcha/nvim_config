a7a9ad5c6a5a72b9414e16d732de67e6af288803
gonzalo velasco <velmon_g@tutanota.com>
2025-07-24 12:25:34 +0200

    Added auto-indent.lua plugin

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -1,9 +1,9 @@
-return { 
-  "catppuccin/nvim", 
-  name = "catppuccin", 
+return {
+  "catppuccin/nvim",
+  name = "catppuccin",
   priority = 1000,
-  config = function() 
+  config = function()
     vim.cmd.colorscheme "catppuccin"
   end
 }
- 
+
--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -17,6 +17,7 @@ return{
       popup_border_style = "rounded",
       enable_git_status = true,
       enable_diagnostics = true,
+      toggle = true,
       open_files_do_not_replace_types = { "terminal", "trouble", "qf" }, -- when opening files, do not use windows containing these filetypes or buftypes
       open_files_using_relative_paths = false,
       sort_case_insensitive = false, -- used when sorting files and directories in the tree
