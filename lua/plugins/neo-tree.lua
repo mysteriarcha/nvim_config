89864d662a096acdef1c14cd6a7c7fd299c5a425
gonzalo velasco <velmon_g@tutanota.com>
2025-07-24 12:01:57 +0200

    Update neo-tree.lua

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -72,7 +72,7 @@ return{
         },
         name = {
           trailing_slash = false,
-          use_git_status_colors = false,
+          use_git_status_colors = true,
           highlight = "NeoTreeFileName",
         },
         git_status = {
